937194fb5697cbbd28359a0115d0074bd4b6cd47
nakamuram <ma510nakamura@icloud.com>
2024-06-26 14:43:35 +0900

    作業用ディレクトリの作成

--- zabbix_agent-4.4.10_install.sh
+++ zabbix_agent-4.4.10_install.sh
@@ -4,7 +4,11 @@
 curl -O https://cdn.zabbix.com/zabbix/binaries/stable/4.4/4.4.10/zabbix_agent-4.4.10-linux-3.0-amd64-static.tar.gz
 
 # 作業用ディレクトリの作成と解凍
-mkdir zabbix_agent-4.4.10 && tar zxvf ./zabbix_agent-4.4.10-linux-3.0-amd64-static.tar.gz -C zabbix_agent-4.4.10
+if [ ! -d "zabbix_agent-4.4.10" ]; then
+  mkdir -p "zabbix_agent-4.4.10"
+fi
+#mkdir zabbix_agent-4.4.10 && tar zxvf ./zabbix_agent-4.4.10-linux-3.0-amd64-static.tar.gz -C zabbix_agent-4.4.10
+tar zxvf ./zabbix_agent-4.4.10-linux-3.0-amd64-static.tar.gz -C zabbix_agent-4.4.10
 
 # Agentの配置
 sudo cp ./zabbix_agent-4.4.10/sbin/zabbix_agentd /usr/sbin/
