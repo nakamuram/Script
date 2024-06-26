67d037acb6766deb765619b2306166785d037363
nakamuram <ma510nakamura@icloud.com>
2024-06-26 14:36:38 +0900

    confの配置まで

--- zabbix_agent-4.4.10_install.sh
+++ zabbix_agent-4.4.10_install.sh
@@ -6,4 +6,14 @@ curl -O https://cdn.zabbix.com/zabbix/binaries/stable/4.4/4.4.10/zabbix_agent-4.
 # 作業用ディレクトリの作成と解凍
 mkdir zabbix_agent-4.4.10 && tar zxvf ./zabbix_agent-4.4.10-linux-3.0-amd64-static.tar.gz -C zabbix_agent-4.4.10
 
+# Agentの配置
+sudo cp ./zabbix_agent-4.4.10/sbin/zabbix_agentd /usr/sbin/
+
+# Confの配置
+if [ ! -d "/etc/zabbix" ]; then
+  sudo mkdir -p "/etc/zabbix"
+fi
+sudo cp ./zabbix_agent-4.4.10/conf/zabbix_agentd.conf /etc/zabbix/
+
+
 
