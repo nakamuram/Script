0c271bc5f322361ea319b4b41db844e70990a46c
nakamuram <ma510nakamura@icloud.com>
2024-06-26 16:26:53 +0900

    pidの設定を変更

--- zabbix_agent-4.4.10_install.sh
+++ zabbix_agent-4.4.10_install.sh
@@ -3,7 +3,7 @@
 # Agentのダウンロード
 echo "curl -O https://cdn.zabbix.com/zabbix/binaries/stable/4.4/4.4.10/zabbix_agent-4.4.10-linux-3.0-amd64-static.tar.gz"
 curl -O https://cdn.zabbix.com/zabbix/binaries/stable/4.4/4.4.10/zabbix_agent-4.4.10-linux-3.0-amd64-static.tar.gz
-echo -e "\n\n\n"
+echo -e "\n\n"
 
 # 作業用ディレクトリの作成と解凍
 if [ ! -d "zabbix_agent-4.4.10" ]; then
@@ -11,27 +11,34 @@ if [ ! -d "zabbix_agent-4.4.10" ]; then
 fi
 echo "tar zxvf ./zabbix_agent-4.4.10-linux-3.0-amd64-static.tar.gz -C zabbix_agent-4.4.10"
 tar zxvf ./zabbix_agent-4.4.10-linux-3.0-amd64-static.tar.gz -C zabbix_agent-4.4.10
-echo -e "\n\n\n"
+echo -e "\n\n"
 
 # Agentの配置
 echo "sudo cp -ia ./zabbix_agent-4.4.10/sbin/zabbix_agentd /usr/sbin/"
 sudo cp -ia ./zabbix_agent-4.4.10/sbin/zabbix_agentd /usr/sbin/
-echo -e "\n\n\n"
+echo -e "\n\n"
 
 # Confの配置
 if [ ! -d "/etc/zabbix" ]; then
   sudo mkdir -p "/etc/zabbix"
 fi
 echo "sudo cp -ia ./zabbix_agent-4.4.10/conf/zabbix_agentd.conf /etc/zabbix/"
+sed -i 's/^LogFile=\/tmp\/zabbix_agentd.log$/\/run\/zabbix\/zabbix_agentd.pid/' ./zabbix_agent-4.4.10/conf/zabbix_agentd.conf
 sudo cp -ia ./zabbix_agent-4.4.10/conf/zabbix_agentd.conf /etc/zabbix/
-echo -e "\n\n\n"
+
+
+echo -e "\n\n"
 
 #Servicesファイルのダウンロードと配置
 echo "curl -O https://raw.githubusercontent.com/nakamuram/Script/main/zabbix-agent.service"
 curl -O https://raw.githubusercontent.com/nakamuram/Script/main/zabbix-agent.service
-echo -e "\n\n\n"
+echo -e "\n\n"
 
 #Servicesファイルの配置
 echo "sudo cp -ia zabbix-agent.service /etc/systemd/system/"
 sudo cp -ia zabbix-agent.service /etc/systemd/system/
-echo -e "\n\n\n"
+echo -e "\n\n"
+
+# systemctl daemon-reload
+# systemctl restart zabbix-agent
+
