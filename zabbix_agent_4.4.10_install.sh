75d72c17759c5e564c8b95fa331b2da3bca50703
nakamuram <ma510nakamura@icloud.com>
2024-06-26 14:19:49 +0900

    Ver1

--- zabbix_agent_4.4.10_install.sh
+++ zabbix_agent_4.4.10_install.sh
@@ -2,3 +2,8 @@
 
 # Agentのダウンロード
 curl -O https://cdn.zabbix.com/zabbix/binaries/stable/4.4/4.4.10/zabbix_agent-4.4.10-linux-3.0-amd64-static.tar.gz
+
+# 作業用ディレクトリの作成と解凍
+mkdir zabbix_agent-4.4.10 && tar zxvf ./zabbix_agent-4.4.10-linux-3.0-amd64-static.tar.gz -C zabbix_agent-4.4.10
+
+
