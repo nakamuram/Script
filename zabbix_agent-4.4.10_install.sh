5173babfe65508a2c11173781005f5b7a7149b6c
nakamuram <ma510nakamura@icloud.com>
2024-06-27 10:11:19 +0900

    最終確認。不要なコメントを削除

--- zabbix_agent-4.4.10_install.sh
+++ zabbix_agent-4.4.10_install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 echo "ZabbixAgentのダウンロード"
-#echo "curl -O https://cdn.zabbix.com/zabbix/binaries/stable/4.4/4.4.10/zabbix_agent-4.4.10-linux-3.0-amd64-static.tar.gz"
 curl -O https://cdn.zabbix.com/zabbix/binaries/stable/4.4/4.4.10/zabbix_agent-4.4.10-linux-3.0-amd64-static.tar.gz
 echo -e "\n\n"
 
@@ -9,12 +8,10 @@ echo "作業用ディレクトリの作成と解凍"
 if [ ! -d "zabbix_agent-4.4.10" ]; then
   mkdir -p "zabbix_agent-4.4.10"
 fi
-#echo "tar zxvf ./zabbix_agent-4.4.10-linux-3.0-amd64-static.tar.gz -C zabbix_agent-4.4.10"
 tar zxvf ./zabbix_agent-4.4.10-linux-3.0-amd64-static.tar.gz -C zabbix_agent-4.4.10
 echo -e "\n\n"
 
 echo "ZabbixAgentの配置"
-#echo "sudo cp -ia ./zabbix_agent-4.4.10/sbin/zabbix_agentd /usr/sbin/"
 sudo cp -ia ./zabbix_agent-4.4.10/sbin/zabbix_agentd /usr/sbin/
 echo -e "\n\n"
 
@@ -22,29 +19,33 @@ echo "ZabbixAgent confの配置"
 if [ ! -d "/etc/zabbix" ]; then
   sudo mkdir -p "/etc/zabbix"
 fi
-#echo "sudo cp -ia ./zabbix_agent-4.4.10/conf/zabbix_agentd.conf /etc/zabbix/"
-#rootで起動可能するおまじない
-# sed -i 's/^# PidFile=\/tmp\/zabbix_agentd.pid$/PidFile=\/run\/zabbix\/zabbix_agentd.pid/' ./zabbix_agent-4.4.10/conf/zabbix_agentd.conf
+#rootで起動可能にするおまじない
 sed -i 's/^# AllowRoot=0/AllowRoot=1/' ./zabbix_agent-4.4.10/conf/zabbix_agentd.conf
 sudo cp -ia ./zabbix_agent-4.4.10/conf/zabbix_agentd.conf /etc/zabbix/
 echo -e "\n\n"
 
 echo "Servicesファイルのダウンロード"
-#echo "curl -O https://raw.githubusercontent.com/nakamuram/Script/main/zabbix-agent.service"
 curl -O https://raw.githubusercontent.com/nakamuram/Script/main/zabbix-agent.service
 echo -e "\n\n"
 
 echo "Servicesファイルの配置"
-#echo "sudo cp -ia zabbix-agent.service /etc/systemd/system/"
 sudo cp -ia zabbix-agent.service /etc/systemd/system/
 echo -e "\n\n"
 
-# サービスを有効にする
+echo "サービスを有効にする"
 sudo systemctl enable zabbix-agent.service
-# デーモンのリロード（設定の再読み込み）
+echo -e "\n\n"
+
+echo "デーモンのリロード（設定の再読み込み）"
 sudo systemctl daemon-reload
-# サービスの再起動
+echo -e "\n\n"
+
+echo "サービスの再起動"
 sudo systemctl restart zabbix-agent
+echo -e "\n\n"
+
+echo "サービスの起動確認"
+sudo systemctl status zabbix-agent
 
 # 以下な感じだとステキかも
 # curl -O https://raw.githubusercontent.com/nakamuram/Script/main/zabbix_agent-4.4.10_install.sh && chmod +x ./zabbix_agent-4.4.10_install.sh && ./zabbix_agent-4.4.10_install.sh
